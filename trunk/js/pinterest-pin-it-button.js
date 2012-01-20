9b61ec82af93857f8fda9598cdd9e9690cb884eb
pderksen <pderksen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-20 06:28:08 +0000

    Moved javascript that fires on button click to a separate file
    git-svn-id: https://plugins.svn.wordpress.org/pinterest-pin-it-button@492519 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/js/pinterest-pin-it-button.js
@@ -0,0 +1,7 @@
+function exec_pinmarklet() {
+    var e=document.createElement('script');
+    e.setAttribute('type','text/javascript');
+    e.setAttribute('charset','UTF-8');
+    e.setAttribute('src','http://assets.pinterest.com/js/pinmarklet.js?r=' + Math.random()*99999999);
+    document.body.appendChild(e);
+}
