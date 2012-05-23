5ce77f057941377856ad8abf127c2a5f3680ede0
pderksen <pderksen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-23 11:53:22 +0000

    Updated to version 1.3.1 (see changelog)
    git-svn-id: https://plugins.svn.wordpress.org/pinterest-pin-it-button/trunk@547998 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/pinterest-pin-it-button-admin.js
+++ js/pinterest-pin-it-button-admin.js
@@ -4,9 +4,6 @@
 jQuery(document).ready(function($) {
 
 	//Enable collapse/expand toggle of admin boxes (like WP dashboard)
-	//Don't use 'dashboard' and 'postbox' scripts built-into WP like WordPress SEO
-	//plugin as they conflict on other pages
-	
 	$(".pib-hndle").toggle(function() {
 		$(this).next(".inside").slideToggle("fast");
 	}, function () {
@@ -19,9 +16,4 @@ jQuery(document).ready(function($) {
 		$(this).next(".pib-hndle").next(".inside").slideToggle("fast");
 	});
 
-	//Submit mailchimp form with link button
-	$("#mc-embedded-subscribe-link").click(function(event) {
-		event.preventDefault();
-		$("#mc-embedded-subscribe-form").submit();
-	});
 });
